b3570c0257950ae360223b8216beeecebd737b58
anthonwellsjo <anthonwellsjo@gmail.com>
2023-02-06 14:37:11 +0100

    feat: with db module

---
+++ src/db.rs
@@ -0,0 +1,76 @@
+use std::error::Error;
+
+use arw_brr::get_app_path;
+use rusqlite::{Connection, Result};
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct AuthSession {
+    pub access_token: Option<String>,
+    pub token_type: Option<String>,
+    pub scope: Option<String>,
+}
+
+/// All db methonds for related to authentication
+pub struct Auth;
+
+impl Auth {
+    pub fn save_token(session: AuthSession) -> Result<AuthSession> {
+        println!("save token");
+
+        let conn = Self::get_db_connection()?;
+
+        conn.execute(
+            "INSERT INTO auth (access_token, token_type, scope) values (?1, ?2, ?3)",
+            &[&session.access_token, &session.token_type, &session.scope],
+        )?;
+
+        conn.close()
+            .unwrap_or_else(|_| panic!("Panicking while closing conection."));
+
+        Ok(session)
+    }
+
+    ///  Gets connection to DB. This function will create a new DB if
+    ///  not already present
+    pub fn get_db_connection() -> Result<Connection> {
+    println!("get db connection");
+
+        let conn = Connection::open(get_app_path("gnn"))?;
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS auth (
+             id INTEGER PRIMARY KEY,
+             access_token TEXT NOT NULL,
+             token_type TEXT NOT NULL,
+             scope TEXT NOT NULL,
+             created_at TEXT DEFAULT CURRENT_TIMESTAMP
+         )",
+            [],
+        )?;
+        println!("got it");
+        Ok(conn)
+
+    }
+
+    /// Gets the latest used token
+    pub fn get_latest() -> Result<Option<AuthSession>> {
+        let conn = Self::get_db_connection()?;
+
+        let mut stmt = conn.prepare(
+            "SELECT access_token, token_type, scope, MAX(created_at)
+             FROM auth
+             LIMIT 1",
+        )?;
+
+        let auth_map = stmt.query_map([], |row| {
+            Ok(AuthSession {
+                access_token: row.get(0)?,
+                token_type: row.get(1)?,
+                scope: row.get(2)?,
+            })
+        })?;
+
+        let auth = Ok(Some(auth_map.into_iter().next().unwrap()?));
+        auth
+    }
+}
