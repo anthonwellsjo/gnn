b5c3f4ccf6bad9b91e1b2ad42f3fef77b7858df5
anthonwellsjo <anthonwellsjo@gmail.com>
2023-02-06 14:37:53 +0100

    feat: implement main app flow

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 mod auth;
+mod db;
+mod app;
 
-use auth::{auth, has_valid_session};
+use auth::{authenticate, has_valid_session};
 
 #[tokio::main]
 async fn main() {
     if !has_valid_session().await {
-        auth().await;
+        authenticate().await.unwrap();
     }
+
+    let action = arw_brr::get_argument_at(0).unwrap();
+    let action = app::Action::from_string(&action);
+    let argument = arw_brr::get_argument_at(1);
+
+    let mut session = app::Session::new(); 
+    session.run(action, argument);
+
+    for res in session.action_responses.iter(){
+        // display_action_response(res);
+        println!("{:?}", res);
+    }
+
 }
